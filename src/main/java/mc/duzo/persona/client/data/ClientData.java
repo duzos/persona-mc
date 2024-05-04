52638d933643d55913be2ff1a22b5d4529566e66
James Hall <james.hall01@outlook.com>
2024-05-04 12:04:56 +0100

    Touch mask on persona reveal + ClientData#onUpdatePlayerData

--- src/main/java/mc/duzo/persona/client/data/ClientData.java
+++ src/main/java/mc/duzo/persona/client/data/ClientData.java
@@ -3,18 +3,19 @@ package mc.duzo.persona.client.data;
 import mc.duzo.persona.PersonaMod;
 import mc.duzo.persona.client.battle.ClientBattleCache;
 import mc.duzo.persona.client.battle.data.ClientBattleData;
+import mc.duzo.persona.client.render.animation.player.PlayerAnimationHelper;
+import mc.duzo.persona.client.render.animation.player.PlayerAnimations;
 import mc.duzo.persona.common.persona.AbstractPersona;
 import mc.duzo.persona.data.PlayerData;
 import mc.duzo.persona.data.ServerData;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.nbt.NbtCompound;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * The clients version of {@link ServerData}
@@ -38,7 +39,33 @@ public class ClientData {
 
     public static void addPlayer(UUID uuid, NbtCompound data) {
         PlayerData playerData = PlayerData.createFromNbt(data);
+
+        PlayerData stale = getInstance().players.get(uuid);
+
         getInstance().players.put(uuid, playerData);
+
+        onUpdatePlayerData(uuid, stale, playerData);
+    }
+    private static void onUpdatePlayerData(UUID playerId, @Nullable PlayerData stale, PlayerData updated) {
+        if (stale != null) {
+            if (stale.isPersonaRevealed() != updated.isPersonaRevealed()) {
+                // The persona must have been hidden / revealed
+                // Play the touch mask anim
+                findPlayer(playerId).ifPresent(player -> PlayerAnimationHelper.playAnimation(player, PlayerAnimations.PERSONA_BATTLE_MASK_TOUCH));
+            }
+        }
+    }
+    private static Optional<AbstractClientPlayerEntity> findPlayer(UUID uuid) {
+        ClientWorld world = MinecraftClient.getInstance().world;
+
+        if (world == null) {
+            PersonaMod.LOGGER.error("Tried to get players from world without a client world!");
+            return Optional.empty();
+        }
+
+        if (!(world.getPlayerByUuid(uuid) instanceof AbstractClientPlayerEntity)) return Optional.empty();
+
+        return Optional.of((AbstractClientPlayerEntity) world.getPlayerByUuid(uuid));
     }
 
     public static PlayerData getPlayerState(UUID uuid) {
